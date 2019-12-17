90fb1b5f923669c6d3b1f41f41500c87778e5497
relykstm <relykstm@gmail.com>
2019-12-16 16:13:20 -0800

    first two exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+function repeatString(string, number) {
+        if (number < 0){
+            return "ERROR";
+        }    
 
-}
+        var newString = '';
+
+        for (i=0; i<number; i++){
+            newString += string;
+        }
+
+        return newString;
+    }
 
 module.exports = repeatString
